1921925571c7504616f5710798fa1970faa9a78a
Shahzada Shah <sshah2@albany.edu>
2020-02-25 21:04:39 -0500

    Add files via upload

--- KabobShackApp.java
+++ KabobShackApp.java
@@ -3,19 +3,21 @@ package shahkabobshack;
 import java.util.Scanner;
 
 public class KabobShackApp {
-	
-	public static void main(String [] args) {
+
+	public static void main(String[] args) {
 		System.out.print("--------------------------------------------------\n");
 		System.out.print(" How many customers are in line: ");
 		Scanner input = new Scanner(System.in);
 		int numCustomers = input.nextInt();
-		
-				// create array for new students
-				Customers [] customers = new Customers[numCustomers];
-				for (int i = 0; i < numCustomers; i++) {
-					System.out.print("--------------------------------------------------\n");
-					customers[i] = new Customers();
-				}
-		
+		while (!(numCustomers > 0)) {
+			System.out.print(" Excuse me? That isn't a valid number try again! ");
+			numCustomers = input.nextInt();
+		}
+		// create array for new students
+		Customers[] customers = new Customers[numCustomers];
+		for (int i = 0; i < numCustomers; i++) {
+			System.out.print("--------------------------------------------------\n");
+			customers[i] = new Customers();
+		}
 	}
 }
